f46076444d43bcd27f4c3a91d46e7a0e1020e406
benhakimilyass <ilyassbenhakim2@gmail.com>
2020-09-07 09:50:15 +0100

    Exporting app

---
+++ _next/static/6lABzdTByp0YVgyvUCjms/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/index":[a]}}("static\u002Fchunks\u002F05d954cf.63b9ae257fe725f585dd.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
