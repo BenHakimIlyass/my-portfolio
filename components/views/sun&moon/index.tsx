0acac1e0b343b3604349a2faccc40fe1a9c04f50
benhakimilyass <ilyassbenhakim2@gmail.com>
2020-09-14 21:57:48 +0100

    Adding animation

--- components/views/sun&moon/index.tsx
+++ components/views/sun&moon/index.tsx
@@ -4,31 +4,46 @@ import { motion, AnimatePresence } from "framer-motion";
 
 import Night from "./night";
 import Midday from "./midday";
-import { AnimatedP } from "../../";
+import { AnimatedP, P as Paragraph, Vstack } from "../../";
+import { useOnScreen } from "../../../hooks";
+
 export default function App() {
   const [dark, toggle] = React.useState(true);
+  const [ref, isOnScreen] = useOnScreen();
 
   return (
-    <Playground
-      onTap={() => toggle((prev) => !prev)}
-      animate={{
-        backgroundImage: dark
-          ? "linear-gradient(180deg,  #051728,#173756)"
-          : "linear-gradient(180deg, #FFFFFF, #FFFAEB)",
-        boxShadow: dark
-          ? "0px 0px 16px rgba(0, 0, 0, 0.24)"
-          : "0px 0px 16px rgba(152, 163, 183, 0.24)",
-      }}
-      whileTap={{ scale: 0.9, backgroundColor: dark ? "#102A43" : "#F4ECD1" }}
-    >
-      <AnimatePresence exitBeforeEnter>
-        {dark ? <Night /> : <Midday />}
-      </AnimatePresence>
+    <Vstack space={2}>
+      <Paragraph direction="center">
+        Here is a tiny cute animation, made by me
+      </Paragraph>
+      <div ref={ref} style={{ height: "90vh" }}>
+        {isOnScreen && (
+          <Playground
+            onTap={() => toggle((prev) => !prev)}
+            animate={{
+              backgroundImage: dark
+                ? "linear-gradient(180deg,  #051728,#173756)"
+                : "linear-gradient(180deg, #FFFFFF, #FFFAEB)",
+              boxShadow: dark
+                ? "0px 0px 16px rgba(0, 0, 0, 0.24)"
+                : "0px 0px 16px rgba(152, 163, 183, 0.24)",
+            }}
+            whileTap={{
+              scale: 0.9,
+              backgroundColor: dark ? "#102A43" : "#F4ECD1",
+            }}
+          >
+            <AnimatePresence exitBeforeEnter>
+              {dark ? <Night /> : <Midday />}
+            </AnimatePresence>
 
-      <P animate={{ color: !dark ? "#BCCCDC" : "#DFF7FA" }}>
-        Tap here to toggle time
-      </P>
-    </Playground>
+            <P animate={{ color: !dark ? "#BCCCDC" : "#DFF7FA" }}>
+              Tap here to toggle time
+            </P>
+          </Playground>
+        )}
+      </div>
+    </Vstack>
   );
 }
 const Playground = styled(motion.div)`
